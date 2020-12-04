bd8ceb66044d87b2ece0d033c797d415d39aa0d0
Davidxy9 <davidwilliamlima@hotmail.com>
2020-12-04 08:23:18 -0300

    styles-components add

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Title = styled.h1`
+    font-size: 48px;
+    color: #3a3a3a;
+    max-width: 450px;
+    line-height: 56px;
+
+    margin-top: 80px;
+    margin-left: 40px;
+
+`;
