4bc8719614fc0e5a7b9511a693ee91148e76c818
Davidxy9 <davidwilliamlima@hotmail.com>
2020-12-04 17:18:25 -0300

    useEffect and your configs add

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,41 +1,61 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-//import api from '../../services/api';
+import api from '../../services/api';
 
 
 import logoImg from '../../assets/logo.svg';
 
+import { Title, Grids } from './styles';
 
-import { Title, Form, Repositories } from './styles';
+interface language {
+  id: string;
+  name: string;
+  forks_url: string;
+}
+//import axios from 'axios';
 
 const Dashboard: React.FC = () => {
-  const [newRepo, setNewRepo] = useState('');
+
+  const [languages, setLanguages] = useState<language[]>([]);
+
+  useEffect(() => {
+    api.get('https://api.github.com/search/repositories?q=java:???&sort=stars&page=1').then(response => {
+      setLanguages(response.data.items);
+    })
+
+  }, [languages])
+
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios</Title>
-
-      <Form>
+      {/*
+      <Form onSubmit={handleAddRepositories}>
         <input
           value={newRepo}
           onChange={(e) => setNewRepo(e.target.value)}
-          placeholder="Digite o nome do repositório" />
+          placeholder="Digite a linguagem" />
         <button type="submit">Pesquisar</button>
-      </Form>
+      </Form> */}
 
-      <Repositories>
-        <a href="test">
-          <img src="https://avatars3.githubusercontent.com/u/55166538?s=460&u=0d608fa19290cb96c761ef39af93a91f67882227&v=4"
-            alt="Davin"
-          />
-          <div>
-            <strong>Davidxy9/GoBarberApp</strong>
-            <p>Projeto de serviços de uma barbearia</p>
-          </div>
 
-          <FiChevronRight size={20} />
-        </a>
-      </Repositories>
+      <Grids>
+        {languages.map(language => (
+
+          <a key={language.id} href="/#">
+
+            <div>
+              <strong>{language.name}</strong>
+              <p>{language.forks_url}</p>
+
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+
+      </Grids>
     </>
   );
 }
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -47,7 +47,7 @@ export const Form = styled.form`
   }
 `;
 
-export const Repositories = styled.div`
+export const Grids = styled.div`
    margin-top: 80px;
    max-width: 700px;
 
